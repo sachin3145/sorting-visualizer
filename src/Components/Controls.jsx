b7f2b372478527dee0fc4268c045bb49dd993608
unknown <sachin3145@outlook.com>
2023-05-26 09:18:35 +0530

    added sorting functionality

--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -1,25 +1,63 @@
 import { getArray } from "./utility";
 import { useArray, useArrayUpdate } from "./ArrayContext";
+import { getBubbleSortAnimations, getHeapSortAnimations, getInsertionSortAnimations, getMergeSortAnimations, getQuickSortAnimations, getSelecttionSortAnimations, getShellSortAnimations } from "./sortingAlgorithms";
 function genNewArray(f) {
-  console.log(f);
   f(getArray(Math.floor((0.75 * window.innerWidth) / 4), 20, 65));
 }
 
-function updateArray(array, i, v, f) {
+function updateArrayValue(array, i, v, f) {
   // can be used to update single of the state array
   let myArr = array.slice();
   myArr[i] = v;
   f(myArr);
 }
 
+function swapArrayValues(array, i, j, f) {
+  // can be used to swap two values in the state array
+  let myArr = array.slice();
+  [myArr[i], myArr[j]] = [myArr[j], myArr[i]];
+  f(myArr);
+}
+
+// const asyncWait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 function NavBar() {
 
-  const setArray = useArrayUpdate();
-  const array = useArray();
+    const setArray = useArrayUpdate();
+    const array = useArray();
+
+  function animate(animations) {
+    // add required delay
+    for (let i = 0; i < animations.length; ++i){
+      if (animations[i].type === "comp") {
+        // comp code
+      }
+      else if (animations[i].type === "swap") {
+        swapArrayValues(array, animations[i].first, animations[i].second, setArray);
+      }
+      else if (animations[i].type === "update") {
+        updateArrayValue(
+          array,
+          animations[i].index,
+          animations[i].val,
+          setArray
+        );
+      }
+    }
+  }
+
+
+
     return (
       <nav id="NavBar">
         <button id="genNewArray" onClick={() => { genNewArray(setArray); }}>generateNewArray</button>
-        <button id="removeFirstBar" onClick={() => { updateArray(array, 0, 0, setArray) }}>removeFirstBar</button>
+        <button id="selectionSort" onClick={() => { animate(getSelecttionSortAnimations(array).animations) }}>selectionSort</button>
+        <button id="bubbleSort" onClick={() => { animate(getBubbleSortAnimations(array).animations) }}>bubbleSort</button>
+        <button id="insertionSort" onClick={() => { animate(getInsertionSortAnimations(array).animations) }}>insertionSort</button>
+        <button id="shellSort" onClick={() => { animate(getShellSortAnimations(array).animations) }}>shellSort</button>
+        <button id="mergeSort" onClick={() => { animate(getMergeSortAnimations(array).animations) }}>mergeSort</button>
+        <button id="quickSort" onClick={() => { animate(getQuickSortAnimations(array).animations) }}>quickSort</button>
+        <button id="heapSort" onClick={() => { animate(getHeapSortAnimations(array).animations) }}>heapSort</button>
       </nav>
     );
 }
--- src/Components/sortingAlgorithms.js
+++ src/Components/sortingAlgorithms.js
@@ -1,24 +1,205 @@
+// bubble sort
+function getBubbleSortAnimations(array) {
+  const animations = [];
+  for (let i = 0; i < array.length - 1; ++i) {
+    for (let j = 0; j < array.length - i - 1; ++j) {
+      animations.push({ type: "comp", first: j, second: j + 1 });
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        animations.push({ type: "swap", first: j, second: j + 1 });
+      }
+    }
+  }
+  return { animations: animations, resultantArray: array };
+}
+
+// selection sort
 function getSelecttionSortAnimations(array) {
-  const animations = []; //[compARR, SWAP, compArr, SWAP ...]
+  const animations = [];
   let min;
   for (let i = 0; i < array.length - 1; ++i) {
-    const comp = [];
     min = i;
     for (let j = i + 1; j < array.length; ++j) {
-      comp.push([min, j]);
-      comp.push([min, j]);
+      animations.push({ type: "comp", first: min, second: j });
       if (array[min] > array[j]) {
         min = j;
       }
     }
     [array[i], array[min]] = [array[min], array[i]];
-    animations.push(comp);
-    animations.push([
-      [i, array[i]],
-      [min, array[min]],
-    ]);
+    animations.push({ type: "swap", first: i, second: min });
   }
-  return {animations: animations, resultantArray:array};
+  return { animations: animations, resultantArray: array };
 }
 
-export { getSelecttionSortAnimations };
+
+// insertion sort
+function getInsertionSortAnimations(array) {
+  const animations = [];
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i],
+      j = i - 1;
+    animations.push({ type: "comp", first: i, second: j });
+    while (j > -1 && array[j] > key) {
+      array[j + 1] = array[j];
+      animations.push({ type: "update", index: j + 1, val: array[j + 1] });
+      j--;
+      if (j === -1) {
+        animations.push({ type: "comp", first: i, second: 0 });
+        break;
+      }
+      animations.push({ type: "comp", first: i, second: j });
+    }
+    array[j + 1] = key;
+    animations.push({ type: "update", index: j + 1, val: array[j + 1] });
+  }
+  return { animations: animations, resultantArray: array };
+}
+
+// shell sort
+function getShellSortAnimations(array) {
+  const animations = [];
+  let j,
+    temp,
+    n = array.length;
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; ++i) {
+      temp = array[i];
+      j = i;
+      animations.push({ type: "comp", first: j - gap, second: i });
+      for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+        array[j] = array[j - gap];
+        animations.push({ type: "update", index: j, val: array[j] });
+        animations.push({ type: "comp", first: j - gap, second: i });
+      }
+      array[j] = temp;
+      animations.push({ type: "update", index: j, val: array[j] });
+    }
+  }
+  return { animations: animations, resultantArray: array };
+}
+
+
+// merge sort
+function getMergeSortAnimations(array) {
+  const animations = [];
+  const auxillaryArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxillaryArray, animations);
+  return { animations: animations, resultantArray: array };
+}
+
+function mergeSortHelper(arr, l, r, aux, animations) {
+  if (l === r) return;
+  const mid = Math.floor(l + (r - l) / 2);
+  mergeSortHelper(aux, l, mid, arr, animations);
+  mergeSortHelper(aux, mid + 1, r, arr, animations);
+  merge(arr, l, mid, r, aux, animations);
+}
+
+function merge(arr, l, mid, r, aux, animations) {
+  let i = l,
+    j = l,
+    k = mid + 1;
+  while (j < mid + 1 && k < r + 1) {
+    animations.push({ type: "comp", first: j, second: k });
+
+    // keeping track of index that is being updated along with the new value
+    if (aux[j] > aux[k]) {
+      animations.push({ type: "update", index: i, val: aux[k] });
+      arr[i++] = aux[k++];
+    } else {
+      animations.push({ type: "update", index: i, val: aux[j] });
+      arr[i++] = aux[j++];
+    }
+  }
+
+  while (j < mid + 1) {
+    animations.push({ type: "comp", first: j, second: j });
+    animations.push({ type: "update", index: i, val: aux[j] });
+    arr[i++] = aux[j++];
+  }
+
+  while (k < r + 1) {
+    animations.push({ type: "comp", first: k, second: k });
+    animations.push({ type: "update", index: i, val: aux[k] });
+    arr[i++] = aux[k++];
+  }
+}
+
+// quick sort
+function partition(array, animations, low, high) {
+  let pivot = array[high];
+  let i = low - 1;
+
+  for (let j = low; j < high; j++) {
+    animations.push({ type: "comp", first: j, second: high });
+    if (array[j] <= pivot) {
+      i++;
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    animations.push({ type: "swap", first: i, second: j });
+  }
+  [array[i + 1], array[high]] = [array[high], array[i + 1]];
+  animations.push({ type: "swap", first: i + 1, second: high });
+  return i + 1;
+}
+
+function quickSort(array, animations, low, high) {
+  if (low < high) {
+    const pi = partition(array, animations, low, high);
+    quickSort(array, animations, low, pi - 1);
+    quickSort(array, animations, pi + 1, high);
+  }
+}
+
+function getQuickSortAnimations(array) {
+  const animations = [];
+  quickSort(array, animations, 0, array.length - 1);
+  return { animations: animations, resultantArray: array };
+}
+
+// heap sort
+function heapify(array, animations, n, i) {
+  let largest = i;
+  let l = 2 * i + 1;
+  let r = 2 * i + 2;
+  if (l < n) {
+    animations.push({ type: "comp", first: l, second: largest });
+  }
+  if (l < n && array[l] > array[largest]) largest = l;
+
+  if (r < n) {
+    animations.push({ type: "comp", first: r, second: largest });
+  }
+  if (r < n && array[r] > array[largest]) largest = r;
+
+  animations.push([largest, i]);
+  if (largest !== i) {
+    [array[i], array[largest]] = [array[largest], array[i]];
+    animations.push({ type: "swap", first: i, second: largest });
+    heapify(array, animations, n, largest);
+  }
+}
+
+function getHeapSortAnimations(array) {
+  const animations = [];
+  const n = array.length;
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--)
+    heapify(array, animations, n, i);
+  for (let i = n - 1; i >= 0; i--) {
+    [array[0], array[i]] = [array[i], array[0]];
+    animations.push({ type: "swap", first: i, second: 0});
+    heapify(array, animations, i, 0);
+  }
+  return { animations: animations, resultantArray: array };
+}
+
+
+export {
+  getBubbleSortAnimations,
+  getSelecttionSortAnimations,
+  getInsertionSortAnimations,
+  getShellSortAnimations,
+  getMergeSortAnimations,
+  getQuickSortAnimations,
+  getHeapSortAnimations
+};
