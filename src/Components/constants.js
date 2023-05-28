cd9418a5b6528810ccb6607489af0e3de2faca67
unknown <sachin3145@outlook.com>
2023-05-28 22:31:51 +0530

    added constants.js to allow effective management of constant values

---
+++ src/Components/constants.js
@@ -0,0 +1,19 @@
+const DELAY = {
+  BUBBLESORT: 0,
+  SELECTIONSORT: 0,
+  INSERTIONSORT: 0,
+  SHELLSORT: 0,
+  MERGESORT: 0,
+  QUICKSORT: 0,
+  HEAPSORT: 0,
+  FAST: 0,
+  SLOW: 50
+};
+
+const COLOR = {
+  ACTIVE: "#EEEEEE",
+  PASSIVE: "#00ADB5",
+};
+
+
+export {DELAY, COLOR}
